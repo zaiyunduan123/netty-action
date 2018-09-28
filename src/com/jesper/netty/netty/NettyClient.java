ca3b09a0d6a2683e8e32d7eb5dea84ed765c77c7
zaiyunduan <1778667492@qq.com>
2018-09-28 23:53:36 +0800

    update

--- src/com/jesper/netty/netty/NettyClient.java
+++ src/com/jesper/netty/netty/NettyClient.java
@@ -8,14 +8,25 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class NettyClient {
+
+    private static final int MAX_RETRY = 5;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8000;
+
+
     public static void main(String[] args) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
-        bootstrap.group(group)
+        bootstrap
+                //1、指定线程模型
+                .group(group)
+                //2、指定IO类型为NIO
                 .channel(NioSocketChannel.class)
+                //3、IO逻辑处理
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
@@ -23,11 +34,26 @@ public class NettyClient {
                     }
                 });
 
-        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
 
-        while (true) {
-            channel.writeAndFlush(new Date() + ": hello world!");
-            Thread.sleep(2000);
-        }
+        // 4.建立连接
+        connect(bootstrap, HOST, PORT, MAX_RETRY);
+    }
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit
+                        .SECONDS);
+            }
+        });
     }
 }
